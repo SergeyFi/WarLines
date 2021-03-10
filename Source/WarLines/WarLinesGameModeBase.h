1d2c41dd9f120a7d2090b9ebd4f023268e9d23fc
Sergey Firsov <fearthunder@yandex.ru>
2021-03-10 23:25:53 +0300

    Init repo

---
+++ Source/WarLines/WarLinesGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WarLinesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WARLINES_API AWarLinesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
